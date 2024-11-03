58e319613852fcf5010812173aafb8131d7de01a
Xinyi Wang <rena.wxy@gmail.com>
2024-11-04 01:26:00 +0900

    Delete css

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
   event.preventDefault(); // 防止表单提交
   alert('消息已发送！感谢你的联系。');
 });
+
+particlesJS.load('particles-js', 'path/to/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
